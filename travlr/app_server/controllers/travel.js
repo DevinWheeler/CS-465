cb79d0af1330c43cf67b4c3ec4a36aa659d20b52
DevinWheeler <devin.wheeler@snhu.edu>
2025-02-09 21:11:20 -0800

    Module 5 completed baseline

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,11 +1,33 @@
 /* Get Travel Views */
 
-var fs = require('fs');
-var trips =JSON.parse(fs.readFileSync('data/trips.json','utf8'));
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
+}
 
-const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' , trips});
-};
+// var fs = require('fs');
+// var trips =JSON.parse(fs.readFileSync('data/trips.json','utf8'));
+
+const travel = async function (req, res , next) {
+    await fetch(tripsEndpoint, options)
+    .then(res => res.json())
+    .then(json => {
+        let message = null;
+        if (!(json instanceof Array)) {
+            message = 'API look up error';
+            json = [];
+        } else {
+            if (!json.length) {
+                message = 'No trips found';
+            }
+        }
+        res.render('travel', { title: 'Travlr Getaways', trips: json });
+    })
+    .catch(err => status(500).send(e.message));
+}
 
 module.exports = {
     travel
